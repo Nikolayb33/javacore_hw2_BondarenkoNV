2b9f8929ddb1b18fd1186d759d8978f4b869cd87
Nikolayb33 <nikolayb33@gmail.com>
2024-09-01 15:56:47 +0300

    start hw2 jc

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+//    Написать метод, возвращающий количество чётных элементов массива.
+//    countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+
+}
